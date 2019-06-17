f65e77f746c85b6d95fd36ea96b6c6a0a83751c6
SeungTaeKim <equus3144@gmail.com>
2019-06-17 17:40:57 +0900

    LCD Light Is Working

--- pople/pople.ino
+++ pople/pople.ino
@@ -1,16 +1,20 @@
-#include<LiquidCrystal.h>
 
+#include <LiquidCrystal_I2C.h>
+
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup() {
   // put your setup code here, to run once:
-  lcd.begin(16,2);
-  lcd.clear();
-  Serial.begin(9600);
+  lcd.begin();
+  lcd.backlight();
+  lcd.print("Hello, World!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-lcd.setCursor(0,0);
+  
 
 }
